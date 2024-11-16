7e868e8e04ecc68e82bf87433a6301f67bf5111c
ungra <junhee_lee2@tmax.co.kr>
2024-11-16 20:50:55 +0900

    metadata

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,9 @@
 import Navigation from "../components/navigation";
 
+export const metadata = {
+  title: "Home",
+};
+
 export default function AboutUs() {
   return (
     <div>
--- app/about-us/company/page.tsx
+++ app/about-us/company/page.tsx
@@ -1,3 +1,7 @@
+export const metadat = {
+  title: "About us",
+};
+
 export default function Company() {
   return <h1>Hello Company</h1>;
 }
