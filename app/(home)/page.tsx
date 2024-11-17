91cbd3754aa4bce0a0119b35ee6a23bff0005c0f
ungra <junhee_lee2@tmax.co.kr>
2024-11-17 10:25:35 +0900

    server component fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,26 @@
+import { resolve } from "path";
+import MovieDetail from "../(movies)/movies/[id]/page";
+
 export const metadata = {
   title: "Home",
 };
 
-export default function Home() {
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+async function getMovies() {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 5000);
+  });
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
       <h1>Hello Home!!</h1>
+      <p>{JSON.stringify(movies)}</p>
     </div>
   );
 }
