5ad13cf0cf9671bed89f02aa53bb85dd44c903a1
ungra <junhee_lee2@tmax.co.kr>
2024-11-18 01:00:59 +0900

    movie styles

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import styles from "../../styles/home.module.css";
+import Movie from "../../components/movie";
 
 export const metadata = {
   title: "Home",
@@ -17,12 +18,13 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div>
-      <h1>HOME PAGE</h1>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movies.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
       ))}
     </div>
   );
---
+++ components/movie.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import styles from "../styles/movie.module.css";
+
+interface IMovieProps {
+  title: string;
+  poster_path: string;
+  id: string;
+}
+
+export default function Movie({ title, poster_path, id }: IMovieProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/movies/${id}`);
+  };
+  return (
+    <div className={styles.movie}>
+      <img src={poster_path} alt={title} onClick={onClick} />
+      <Link href={`/movies/${id}`}>{title}</Link>
+    </div>
+  );
+}
